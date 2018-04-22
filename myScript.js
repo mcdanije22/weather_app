a33d107605e589ea253f2ae580e5f3765c6f74c2
mcdanije22 <35205212+mcdanije22@users.noreply.github.com>
2018-04-22 18:53:06 -0400

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
 var lat ="lat=40.8041667";
 var long = "lon=-73.9436111";
+var currentWeather;
 /*function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -18,9 +19,15 @@ function getData() {
 
 xhr.onload = function(){
   var data = JSON.parse(xhr.responseText);
-    document.getElementById("x").innerHTML = data.main.temp;
-      document.getElementById("z").innerHTML = data.weather[0].description;
-    document.getElementById("y").innerHTML = xhr.responseText;
+  currentWeather = Math.round(data.main.temp);
+    document.getElementById("temp").innerHTML = currentWeather + " &#8457;";
+    document.getElementById("con").innerHTML = data.weather[0].main;
+    document.getElementById("name").innerHTML = data.name +", "+ data.sys.country;
+    //document.getElementById("test").innerHTML = data.weather[0].icon;
+    var icon =  data.weather[0].icon;
+    var link = "http://openweathermap.org/img/w/" + icon + ".png";
+    document.getElementById("icon").src = link;
+  //  document.getElementById("y").innerHTML = xhr.responseText;
   console.log(xhr.responseText);
 }
 var api = "https://api.openweathermap.org/data/2.5/weather?";
@@ -32,5 +39,9 @@ xhr.open("GET",url, true);
 xhr.send();
 }
 window.onload = getData;
+function show () {
+currentWeather = Math.round((currentWeather - 32) * 5/9);
+document.getElementById("temp").innerHTML = currentWeather + "&#8451;";
+}
 //var today = new Date();
 //document.getElementById('time').innerHTML=today;
